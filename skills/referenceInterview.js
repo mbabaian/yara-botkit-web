80a72d164bcd5333d62b7c182a3b94fc653eb35b
Monica Babaian <monica.babaian@gmail.com>
2018-04-08 16:42:16 -0500

    Restore previous version

---
+++ skills/referenceInterview.js
@@ -0,0 +1,11 @@
+module.exports = function(controller) {
+    // controller.hears('fiction', 'message_received', function(bot, message) {
+    //     bot.reply(message, 'I need more information like a specific genre. I can also search by title ' +
+    //     'or author\'s name.')
+    // })
+
+    // // nonfiction needs regex to avoid getting confused with 'no'
+    // controller.hears(/nonfiction/i, 'message_received', function(bot, message) {
+    //     bot.reply(message, 'I need more information before I can search. Please give me a nonfiction subject or topic.')
+    // })
+}
